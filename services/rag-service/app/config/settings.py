cc93cdd21d82bbc58ec78a4f5e10470004d2183c
Sheng Wei <139607382+shengweikoh@users.noreply.github.com>
2026-02-23 22:39:19 +0800

    Bedrock llm hotfix (#51)
    * use bedrock llm instead of google gemini
    
    * Go back to using google for now. aws bedrock still buggy

--- services/rag-service/app/config/settings.py
+++ services/rag-service/app/config/settings.py
@@ -30,7 +30,7 @@ class Settings(BaseSettings):
     ]
 
     # AWS Bedrock
-    AWS_REGION: str = "us-west-1"
+    AWS_REGION: str = "ap-southeast-1"
     AWS_ACCESS_KEY_ID: Optional[str] = None
     AWS_SECRET_ACCESS_KEY: Optional[str] = None
     BEDROCK_EMBED_MODEL_ID: str = "amazon.nova-2-embedding-v1"
