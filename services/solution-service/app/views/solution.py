163787e40b38cbd6926cfb514619d6acd57c6b1a
shengweikoh <shengweikoh.2023@scis.smu.edu.sg>
2026-03-06 23:42:28 +0800

    change timestamp type

--- services/solution-service/app/views/solution.py
+++ services/solution-service/app/views/solution.py
@@ -72,7 +72,7 @@ async def create_solutions_table():
         reference_event TEXT,
         solution_description TEXT,
         scores INTEGER NOT NULL,
-        create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        create_time TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
         
         -- Foreign key constraint
         CONSTRAINT fk_solutions_exception_id 
